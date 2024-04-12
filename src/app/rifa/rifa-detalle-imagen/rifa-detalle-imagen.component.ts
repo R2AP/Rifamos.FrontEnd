a5dff943f6074527486f46d34fc7a1728f932747
Carlos <cars15_@hotmail.com>
2024-04-11 21:58:13 -0500

    Implementacion de imagen

--- src/app/rifa/rifa-detalle-imagen/rifa-detalle-imagen.component.ts
+++ src/app/rifa/rifa-detalle-imagen/rifa-detalle-imagen.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { RifaService } from '../../core/services/rifa.service';
 import { CommonModule } from '@angular/common';
 import { Base64 } from 'js-base64';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-rifa-detalle-imagen',
@@ -15,30 +16,34 @@ export class RifaDetalleImagenComponent implements OnInit {
   private readonly rifaSvc = inject(RifaService)
   rifa: any = [];
   ImageDataUrl64: string = "";
+  base64Image: string = "";
 
   rifaId: number = 0;
-  // rifa$ = this.rifaSvc.obtenerRifaDetalle();
+
+  constructor(private sanitizer:DomSanitizer){
+  }
 
   ngOnInit(): void {
     this.rifaId = this.rifaIdInput;
-    console.log ('ngOnInit this.rifaId rifa-detalle-imagen: ' + this.rifaId);
-    //this.obtenerRifaDetalle(this.rifaId);
+    this.obtenerRifaDetalle(this.rifaIdInput);
   }
 
   obtenerRifaDetalle(rifaId:number)
   {
     this.rifaSvc.obtenerRifaDetalle(rifaId).subscribe({
       next:(res: any) => {
-        console.log ('this.rifa 1');
         this.rifa = res;
-        console.log ('this.rifa 2');
         console.log ( res );
-        this.ImageDataUrl64 = "data:image/png;base64," + Base64.extendString()
-        console.log ('this.rifa 3');
-
+        // this.ImageDataUrl64 = "data:image/png;base64," + Base64.extendString()
+        this.base64Image = 'data:image/png;base64, ' + res.imagen;
+        this.transform()
       },
       error:(error) => console.log('Error consultando la rifa', error)
     })
   }
+
+  transform(){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.base64Image);
+}
 
 }
