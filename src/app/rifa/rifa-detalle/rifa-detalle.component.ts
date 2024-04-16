282877b03be86f5d6c75bd016ac02e5a6042a4ea
Carlos <cars15_@hotmail.com>
2024-04-16 08:32:49 -0500

    cambios en estilo

--- src/app/rifa/rifa-detalle/rifa-detalle.component.ts
+++ src/app/rifa/rifa-detalle/rifa-detalle.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RifaDetalleDatosComponent } from '../rifa-detalle-datos/rifa-detalle-datos.component';
 import { RifaDetalleImagenComponent } from '../rifa-detalle-imagen/rifa-detalle-imagen.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-rifa-detalle',
   standalone: true,
-  imports: [RifaDetalleDatosComponent, RifaDetalleImagenComponent],
+  imports: [FormsModule,MatButtonModule, MatDividerModule,MatFormFieldModule,MatInputModule,RifaDetalleDatosComponent, RifaDetalleImagenComponent],
   templateUrl: './rifa-detalle.component.html',
   styleUrl: './rifa-detalle.component.css'
 })
