ec1a6a5922417159ac549a8b78526d85cc37c24e
Carlos <cars15_@hotmail.com>
2024-04-17 00:31:53 -0500

    Inicio del iniciar sesion

--- src/app/auth/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/auth/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatDividerModule],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
