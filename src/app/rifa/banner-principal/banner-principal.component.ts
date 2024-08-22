71ba102a85ca9e36e72be224609c4411ff58ad4f
Carlos <cars15_@hotmail.com>
2024-08-22 03:26:39 -0500

    Se agrega el botón y las imágenes al banner principal

--- src/app/rifa/banner-principal/banner-principal.component.ts
+++ src/app/rifa/banner-principal/banner-principal.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-banner-principal',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatIconModule],
   templateUrl: './banner-principal.component.html',
   styleUrl: './banner-principal.component.css'
 })
