f16f2128eaa6dc0636d0346f913b501691116cc5
Carlos <cars15_@hotmail.com>
2024-08-24 02:21:16 -0500

    Integración de banner principal con back e implementación de slide

--- src/app/rifa/banner-principal/banner-principal.component.ts
+++ src/app/rifa/banner-principal/banner-principal.component.ts
@@ -1,23 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RifaService } from '../../core/services/rifa.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-
+import { register } from 'swiper/element/bundle';
+import { DomSanitizer } from '@angular/platform-browser';
+// register Swiper custom elements
+register();
 @Component({
   selector: 'app-banner-principal',
   standalone: true,
   imports: [MatButtonModule,MatIconModule, CommonModule],
   templateUrl: './banner-principal.component.html',
-  styleUrl: './banner-principal.component.css'
+  styleUrl: './banner-principal.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BannerPrincipalComponent implements OnInit{
   private readonly rifaSvc = inject(RifaService);
   private readonly router = inject(Router);
 
-  rifa: any = [];
+  rifas: any = [];
   
+  constructor(private sanitizer:DomSanitizer){
+  }
+
   ngOnInit():void {
     this.obtenerDatosRifa();
   }
@@ -27,7 +34,7 @@ export class BannerPrincipalComponent implements OnInit{
     this.rifaSvc.obtenerListaRifaEstado(2).subscribe({
       next:(res: any) => {
         
-        this.rifa = res;
+        this.rifas = res;
         console.log ( res );
 
       },
@@ -38,5 +45,11 @@ export class BannerPrincipalComponent implements OnInit{
         console.log('Error consultando la rifa', error)
       }
     })
+  }
+
+  transform(imagen:string){
+    let base64Image:string = "";
+    base64Image = 'data:image/png;base64, ' + imagen;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(base64Image);
   }
 }
